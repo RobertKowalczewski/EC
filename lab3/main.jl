0558e8214914054b34e834119c33a1baa2b72dff
RobertKowalczewski <robert.kow12321@gmail.com>
2025-10-26 20:40:55 +0100

    add tspb

--- lab3/main.jl
+++ lab3/main.jl
@@ -2,7 +2,14 @@ include("local-search.jl")
 include("moves.jl")
 include("helpers.jl")
 
+println("TSPA:")
 data_path = "lab1/TSPA.csv"
+distance_matrix, costs = prepare_data(data_path)
+
+test_algorithms(distance_matrix, costs)
+
+println("TSPB:")
+data_path = "lab1/TSPB.csv"
 distance_matrix, costs = prepare_data(data_path)
 
 test_algorithms(distance_matrix, costs)
